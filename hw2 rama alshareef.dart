ee9d0f1c5bd496b342b597e8c829144f649e525e
Rama774 <105730518+Rama774@users.noreply.github.com>
2022-05-19 16:08:25 +0300

    Add files via upload

---
+++ hw2 rama alshareef.dart
@@ -0,0 +1,6 @@
+void main() { 
+   List numbers = [1,2,3,4,5,6]; 
+   print('The value of list before removing the list element ${numbers}'); 
+   bool res = numbers.remove(1); 
+   print('The value of list after removing the list element ${numbers}'); 
+}
